91bbcb85921b675fe4818f6b3b8aa79670f58dc4
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-10-01 01:34:03 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let interval = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function backgroundColor() { 
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+function startColor() { 
+    interval = setInterval(backgroundColor, 1000);
+    
+};
+
+function stopColor() {
+    clearInterval(interval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    
+};
+
+startBtn.addEventListener('click', startColor);
+stopBtn.addEventListener('click', stopColor);
+
 
